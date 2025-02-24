7baa00f4f820ca1d021adc0122ef3b1411f1b599
swag-hana <linaoalzz67@gmail.com>
2025-02-24 13:35:40 +0000

    second commit

--- Midterm_Exam_Application/app/Http/Controllers/Controller.php
+++ Midterm_Exam_Application/app/Http/Controllers/Controller.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = [
+            ['name' => 'Naruto Action Figure', 'price' => 29.99, 'image' => 'naruto.jpg'],
+            ['name' => 'One Piece Luffy Hat', 'price' => 19.99, 'image' => 'luffy_hat.jpg'],
+            ['name' => 'Attack on Titan Hoodie', 'price' => 39.99, 'image' => 'aot_hoodie.jpg'],
+        ];
+
+        return view('products', compact('products'));
+    }
 }
